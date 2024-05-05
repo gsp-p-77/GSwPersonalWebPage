676f61b9aa44a287342f252b89f87502f4777738
gsp-p-77 <gero.sparwald@gmail.com>
2024-05-05 18:56:54 +0200

    Change port back to 3000

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 
 const app = express();
-const port = 80;
+const port = 3000;
 app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.render("index.ejs");
